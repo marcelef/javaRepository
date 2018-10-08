4ac353945d3e92482720a4a9e52860edfcb8558f
Marcel <m.fevrier123@gmail.com>
2018-10-08 13:00:09 +0200

    Added code for exercise 1

---
+++ uk.ac.reading.dy007252.marcelFevrier.Week_2/src/uk/ac/reading/dy007252/marcelFevrier/Week_2/GeneralCode.java
@@ -0,0 +1,14 @@
+package uk.ac.reading.dy007252.marcelFevrier.Week_2;
+
+public class GeneralCode {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable(10);
+		
+		String multiplicationTable = table.toString();
+		
+		System.out.println(multiplicationTable);
+
+	}
+
+}
