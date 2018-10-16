34473629b4f170e397614f57c84ff224aaf20e8d
dy007252 <dy007252@OWL.rdg.ac.uk>
2018-10-16 13:18:44 +0100

    Changed name of the package

--- uk.ac.reading.dy007252.marcelFevrier.Week_3/src/uk/ac/reading/dy007252/marcelFevrier/buildingconsole/Main.java
+++ uk.ac.reading.dy007252.marcelFevrier.Week_3/src/uk/ac/reading/dy007252/marcelFevrier/buildingconsole/Main.java
@@ -1,10 +1,10 @@
-package uk.ac.reading.dy007252.marcelFevrier.Week_3;
+package uk.ac.reading.dy007252.marcelFevrier.buildingconsole;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Test
 	}
 
 }
